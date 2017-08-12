aa5e3ff6f051444edaf33ff9a05707a238378a22
David Giovannini <dave@David.local>
2017-08-12 12:20:37 -0500

    MQTTSessionStream

---
+++ SwiftFog/SwiftFog/mqtt/connection/MQTTConnection.swift
@@ -0,0 +1,12 @@
+//
+//  MQTTConnection.swift
+//  SwiftFog
+//
+//  Created by David Giovannini on 8/12/17.
+//  Copyright © 2017 Object Computing Inc. All rights reserved.
+//
+
+import Foundation
+
+class MQTTConnection {
+}
---
+++ SwiftFog/SwiftFog/mqtt/connection/MQTTPublisher.swift
@@ -0,0 +1,9 @@
+//
+//  MQTTPublisher.swift
+//  SwiftFog
+//
+//  Created by David Giovannini on 8/12/17.
+//  Copyright © 2017 Object Computing Inc. All rights reserved.
+//
+
+import Foundation
