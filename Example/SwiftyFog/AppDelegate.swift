55541ebf6bd0334d81d7d3d96e53fc97ffbe0717
David Giovannini <dave@David.local>
2017-09-03 08:38:31 -0500

    workaround ping issue

--- Example/SwiftyFog/AppDelegate.swift
+++ Example/SwiftyFog/AppDelegate.swift
@@ -38,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Create the concrete MQTTClient to connect to a specific broker
 		// MQTTClient is an MQTTBridge
 		mqtt = MQTTClient(
+			client: MQTTClientParams(keepAlive: 0),
 			host: MQTTHostParams(host: trainName + ".local", port: .standard),
 			auth: MQTTAuthentication(username: "dsjove", password: "password"),
 			reconnect: MQTTReconnectParams(),
