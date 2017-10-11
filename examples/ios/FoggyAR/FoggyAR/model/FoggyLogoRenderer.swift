8f51219ea032cecb17866d47c1b5309bc48c3fef
tobischw <tsgp3@mst.edu>
2017-10-11 13:35:11 -0500

    Edited lightbulb and it now hides/shows based on MQTT broadcasts

---
+++ examples/ios/FoggyAR/FoggyAR/model/FoggyLogoRenderer.swift
@@ -0,0 +1,9 @@
+//
+//  FoggyLogoRenderer.swift
+//  FoggyAR
+//
+//  Created by Tobias Schweiger on 10/11/17.
+//  Copyright © 2017 Object Computing Inc. All rights reserved.
+//
+
+import Foundation
