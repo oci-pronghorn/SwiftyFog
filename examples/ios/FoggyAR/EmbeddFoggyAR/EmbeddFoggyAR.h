9724cf279704610fc3b76e6303c289af46d038e9
David Giovannini <giovanninid@objectcomputing.com>
2017-11-21 14:32:44 -0600

    add library for FoggyAR in playground

---
+++ examples/ios/FoggyAR/EmbeddFoggyAR/EmbeddFoggyAR.h
@@ -0,0 +1,19 @@
+//
+//  EmbeddFoggyAR.h
+//  EmbeddFoggyAR
+//
+//  Created by David Giovannini on 11/21/17.
+//  Copyright © 2017 Object Computing Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmbeddFoggyAR.
+FOUNDATION_EXPORT double EmbeddFoggyARVersionNumber;
+
+//! Project version string for EmbeddFoggyAR.
+FOUNDATION_EXPORT const unsigned char EmbeddFoggyARVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbeddFoggyAR/PublicHeader.h>
+
+
