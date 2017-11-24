24d92fcf3dda6a9b07ec5841cd501b97adda71ff
David Giovannini <dave@Davids-MacBook-Pro.local>
2017-11-23 21:54:21 -0500

    use factory in example

--- examples/playground/FoggyPlaygroundMqtt.playground/Pages/MQTTRouter.xcplaygroundpage/Contents.swift
+++ examples/playground/FoggyPlaygroundMqtt.playground/Pages/MQTTRouter.xcplaygroundpage/Contents.swift
@@ -7,7 +7,7 @@ let router = MQTTRouter()
 
 class Direct: MQTTRouterDelegate {
 	func mqtt(send packet: MQTTPacket, completion: @escaping (Bool)->()) {
-		print("Routing to self\(packet)")
+		print("Routing: \(packet)")
 		router.dispatch(packet: packet)
 	}
 	func mqtt(unhandledMessage: MQTTMessage) {
@@ -15,27 +15,47 @@ class Direct: MQTTRouterDelegate {
 	}
 }
 
-class MasrshaledDirect: MQTTRouterDelegate {
+class MasrshalledDirect: MQTTRouterDelegate {
 	let factory = MQTTPacketFactory()
 	func mqtt(send packet: MQTTPacket, completion: @escaping (Bool)->()) {
-		// TODO marshal and unmarshal using factory
-		router.dispatch(packet: packet)
+		print("Routing: \(packet)")
+		var data = Data()
+		factory.send(packet) { wrapper in
+			wrapper { ptr, l in
+				data = Data(bytes: ptr, count: l)
+				return l
+			}
+		}
+		var cursor = 0
+		var result = UnmarshalState.failedReadHeader
+		data.withUnsafeBytes { (u8Ptr: UnsafePointer<UInt8>) in
+			result = factory.receive { ptr, l in
+				let pos = u8Ptr.advanced(by: cursor)
+				memcpy(ptr, pos, l)
+				cursor += l
+				return l
+			}
+		}
+	
+		if case .success(let packet) = result {
+			router.dispatch(packet: packet)
+		}
 	}
 	func mqtt(unhandledMessage: MQTTMessage) {
-		print("Unhandled \(unhandledMessage)")
+		print("Unhandled: \(unhandledMessage)")
 	}
 }
 
-let connect = Direct()
+let connect = MasrshalledDirect()
 router.delegate = connect
 
 class Business {
 	func receive(_ msg: MQTTMessage) {
-		print("Received \(msg)")
+		print("Received: \(msg)")
 	}
 }
 
 let business = Business()
 var subscription: MQTTBroadcaster? = router.broadcast(to: business, topics: [("hello", .atMostOnce, Business.receive)])
-router.publish(MQTTMessage(topic: "hello"), completion: nil)
+router.publish(MQTTMessage(topic: "hello", payload: "something".data(using: .utf8)!), completion: nil)
 subscription = nil
